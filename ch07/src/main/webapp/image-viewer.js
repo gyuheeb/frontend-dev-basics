84e981418f50262f00feda66a761e623fd0556a7
gyuheeb <gyuhee4529@naver.com>
2023-02-27 20:08:23 +0900

    image slider #finish

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,5 +1,33 @@
+
+$(function(){
+
+	
+	$("#btn-slideshow").click(function(){
+		var interval =setInterval(function(){
+		var index = Math.floor(Math.random()*imageViewer._images.length);
+			$("img").attr('src',imageViewer._images[index].file)
+	},1500); 
+	$("#btn-slideshow").replaceWith("<button id='btn'>stop</button>");
+	$("#btn").click(function(){
+		clearInterval(interval);
+	});
+	
+		
+	});
+	
+	
+	$("#btn-change").click(function(){
+		var index = Math.floor(Math.random()*imageViewer._images.length);
+		$("img").attr('src',imageViewer._images[index].file);
+	});
+	
+	
+	
+	
+});
 imageViewer={
 	init: function(){
+		
 		for(var i = 0; i<100; i++){
 			this._changeImage();
 		}
@@ -8,18 +36,23 @@ imageViewer={
 		var index = Math.floor(Math.random()*this._images.length);
 		console.log(index);
 	},
-	_intervalId: null
-		
+	
+	_intervalId:null
+	
+
+	
+	
 	,
 	_images:[
-		{name: 'Chrysanthemum',file: '/images/Chrysanthemum.jpg'},
-		{name: 'Desert',file: '/images/Desert.jpg'},
-		{name: 'Hydrangeas',file: '/images/Hydrangeas.jpg'},
-		{name: 'Jellyfish',file: '/images/Jellyfish.jpg'},
-		{name: 'Koala',file: '/images/Koala.jpg'},
-		{name: 'Lighthouse',file: '/images/Lighthouse.jpg'},
-		{name: 'Penguins',file: '/images/Penguins.jpg'},
-		{name: 'Tulips',file: '/images/Tulips.jpg'}
+		{name: 'Chrysanthemum',file: 'images/Chrysanthemum.jpg'},
+		{name: 'Desert',file: 'images/Desert.jpg'},
+		{name: 'Hydrangeas',file: 'images/Hydrangeas.jpg'},
+		{name: 'Jellyfish',file: 'images/Jellyfish.jpg'},
+		{name: 'Koala',file: 'images/Koala.jpg'},
+		{name: 'Lighthouse',file: 'images/Lighthouse.jpg'},
+		{name: 'Penguins',file: 'images/Penguins.jpg'},
+		{name: 'Tulips',file: 'images/Tulips.jpg'}
 		]
-	
+
 };
+
