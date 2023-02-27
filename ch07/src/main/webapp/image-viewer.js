1fdb8653d10e830c632ed631442dd00c36a2d402
gyuheeb <gyuhee4529@naver.com>
2023-02-27 12:27:43 +0900

    jQuery #5

---
+++ ch07/src/main/webapp/image-viewer.js
@@ -0,0 +1,13 @@
+imageViewer={
+	_images:[
+		{name: 'Chrysanthemum',file: '/images/Chrysanthemum.jpg'},
+		{name: 'Desert',file: '/images/Desert.jpg'},
+		{name: 'Hydrangeas',file: '/images/Hydrangeas.jpg'},
+		{name: 'Jellyfish',file: '/images/Jellyfish.jpg'},
+		{name: 'Koala',file: '/images/Koala.jpg'},
+		{name: 'Lighthouse',file: '/images/Lighthouse.jpg'},
+		{name: 'Penguins',file: '/images/Penguins.jpg'},
+		{name: 'Tulips',file: '/images/Tulips.jpg'}
+		]
+	
+};
