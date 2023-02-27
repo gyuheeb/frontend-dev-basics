31c47048163017611d6b203035f092c2c85c88af
gyuheeb <gyuhee4529@naver.com>
2023-02-27 15:59:06 +0900

    ajax #2

--- ch08/src/main/java/ch08/WebConfig.java
+++ ch08/src/main/java/ch08/WebConfig.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
